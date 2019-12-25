81014f2eee23836ecf5e3def2484f5850033eb27
iwiedenm <iwiedenm@lion.lmu.edu>
2019-12-24 17:34:26 -0800

    Update createPost

--- src/manager/functions/core/admin/createPost.js
+++ src/manager/functions/core/admin/createPost.js
@@ -31,50 +31,34 @@ let Module = {
 
       // TODO: authenticate admin!
       let authAdmin = await assistant.authorizeAdmin();
-      console.log('IS ADMIN:', authAdmin);
-      console.log('data:', assistant.request.data);
+      let repoInfo = assistant.parseRepo(This.ref.functions.config().github.repo_website);
+      if (!authAdmin) {
+        response.status = 500;
+        response.error = 'Unauthenticated, admin required.';
+        assistant.log(response);
+        return res.status(response.status).json(response);
+      } else {
+        // Poster = Poster || require('/Users/ianwiedenman/Documents/GitHub/ITW-Creative-Works/ultimate-jekyll-poster');
+        Poster = Poster || require('ultimate-jekyll-poster');
 
-      // Poster = Poster || require('/Users/ianwiedenman/Documents/GitHub/ITW-Creative-Works/ultimate-jekyll-poster');
-      Poster = Poster || require('ultimate-jekyll-poster');
+        let poster = new Poster();
 
-      let poster = new Poster();
-      // pathApi = pathApi || require('path');
+        // Save to disk OR commit
+        poster.onDownload = async function (meta) {
+          return new Promise(async function(resolve, reject) {
+            let finalPath = poster.removeDirDot(meta.finalPath);
+            let tempPath = (meta.tempPath);
+            await createFile(This.ref.functions.config().github.user, repoInfo.user, repoInfo.name, This.ref.functions.config().github.key, finalPath, await poster.readImage(tempPath));
+            resolve();
+          });
+        }
 
-      // Save to disk OR commit
-      // poster.onDownload = async function (req, filepath, filename, ext) {
-      poster.onDownload = async function (meta) {
-        return new Promise(async function(resolve, reject) {
-          // let path = `${filepath}${filename}.${ext}`;
-          // let path = meta.tempPath;
-          // console.log('onDownload', path);
-          // req.pipe(fs.createWriteStream(path));
-          // req.pipe(poster.createWriteStream(path, {temp: true}));
-          // await poster.saveImage(path, {temp: true});
-          // await createFile(This.ref.functions.config().github.key, '', '', '', 'api-test/test.txt', `Hello World! ${Math.random()}`);
-          let finalPath = poster.removeDirDot(meta.finalPath);
-          let tempPath = (meta.tempPath);
-          // console.log('-----onDownload READING FROM TO', tempPath);
-          // console.log('-----onDownload POSTING TO', finalPath);
-          await createFile(This.ref.functions.config().github.key, '', '', '', finalPath, await poster.readImage(tempPath));
+        let finalPost = await poster.create(assistant.request.data);
 
-          console.log('...done');
-          resolve();
-        });
+        // Save post OR commit
+        await createFile(This.ref.functions.config().github.user, repoInfo.user, repoInfo.name, This.ref.functions.config().github.key, poster.removeDirDot(finalPost.path), finalPost.content);
       }
-      // let testPath = '/var/folders/jy/s5g0slgn1kz549p52nmk31nm0000gn/T/assets/_src/images/blog/posts/post-7/here-is-an-image.png';
-      // let testPath2 = '/var/folders/jy/s5g0slgn1kz549p52nmk31nm0000gn/T/assets/_src/images/blog/posts/post-7/example-post-7.jpg';
-      // await createFile(This.ref.functions.config().github.key, '', '', '', poster.removeDirDot('assets/_src/images/blog/posts/post-7/example-post-7.jpg'), poster.readImage(testPath2, {temp: true}));
-      // await createFile(This.ref.functions.config().github.key, '', '', '', poster.removeDirDot('assets/_src/images/blog/posts/post-7/here-is-an-image.png'), poster.readImage(testPath, {temp: true}));
-      // return 'break';
 
-      let finalPost = await poster.create(assistant.request.data);
-
-      // Save post OR commit
-      // fs.write(finalPost.path, finalPost.content)
-      await createFile(This.ref.functions.config().github.key, '', '', '', poster.removeDirDot(finalPost.path), finalPost.content);
-
-
-      //
       assistant.log(assistant.request.data, response);
       // return 'break';
       return res.status(response.status).json(response);
@@ -84,7 +68,7 @@ let Module = {
 module.exports = Module;
 
 // HELPERS //
-async function createFile(username, repoUser, repoName, key, path, contents) {
+async function createFile(user, repoUser, repoName, key, path, contents) {
   pathApi = pathApi || require('path');
   let fileParsed = pathApi.parse(path);
 
@@ -96,7 +80,7 @@ async function createFile(username, repoUser, repoName, key, path, contents) {
     try {
 
       // let pathGet = `https://api.github.com/repos/iwiedenm/ultimate-jekyll/git/trees/template:${encodeURIComponent(path_noExt)}`;
-      let pathGet = `https://api.github.com/repos/iwiedenm/ultimate-jekyll/git/trees/template:${encodeURIComponent(pathApi.dirname(path))}`;
+      let pathGet = `https://api.github.com/repos/${repoUser}/${repoName}/git/trees/template:${encodeURIComponent(pathApi.dirname(path))}`;
       console.log('-------GET', pathGet);
       await makeRequest({
         // url: `https://api.github.com/repos/:owner/:repo/contents/:path`,
@@ -108,11 +92,11 @@ async function createFile(username, repoUser, repoName, key, path, contents) {
         timeout: 30000,
         json: true,
         auth: {
-          user: 'iwiedenm',
-          pass: '83cb746a532d907a089101d4d6e4f2c26aa90362'
+          user: user,
+          pass: key
         },
         headers: {
-          'User-Agent': 'iwiedenm'
+          'User-Agent': user
         }
       })
       .then(function (resp) {
@@ -128,7 +112,7 @@ async function createFile(username, repoUser, repoName, key, path, contents) {
     sha = null;
   }
 
-  let pathPut = `https://api.github.com/repos/iwiedenm/ultimate-jekyll/contents/${path}`;
+  let pathPut = `https://api.github.com/repos/${repoUser}/${repoName}/contents/${path}`;
   let writeRequest =
   {
     // url: `https://api.github.com/repos/:owner/:repo/contents/:path`,
@@ -141,11 +125,11 @@ async function createFile(username, repoUser, repoName, key, path, contents) {
     timeout: 30000,
     json: true,
     auth: {
-      user: 'iwiedenm',
-      pass: '83cb746a532d907a089101d4d6e4f2c26aa90362'
+      user: user,
+      pass: key
     },
     headers: {
-      'User-Agent': 'iwiedenm'
+      'User-Agent': user
     }
   }
   if (sha) {
