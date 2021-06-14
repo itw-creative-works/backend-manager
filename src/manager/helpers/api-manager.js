4d7e8efdeace534bee07a2a8fac557d412fe38ce
Ian <ian.wiedenman@gmail.com>
2021-06-13 22:45:30 -0700

    Fixed apimanager

--- src/manager/helpers/api-manager.js
+++ src/manager/helpers/api-manager.js
@@ -194,7 +194,7 @@ ApiManager.prototype.validateOfficialRequest = async function (assistant, apiUse
   let isOfficial = self.options.officialAPIKeys.includes(data.apiKey);
   assistant.ref.Manager.libraries.hcaptcha = assistant.ref.Manager.libraries.hcaptcha || assistant.ref.Manager.require('hcaptcha');
   const hcaptcha = assistant.ref.Manager.libraries.hcaptcha;
-  
+
   if (self.options.officialAPIKeys.includes(data.apiKey)) {
       const captchaResult = await hcaptcha.verify(process.env.HCAPTCHA_SECRET, data['h-captcha-response'])
         .then((data) => data)
@@ -208,7 +208,7 @@ ApiManager.prototype.validateOfficialRequest = async function (assistant, apiUse
           verified: false,
         }
       } else {
-        ApiManager.incrementUserStat(apiUser, 'requests', -1);
+        self.incrementUserStat(apiUser, 'requests', -1);
         return {
           ok: true,
           official: true,
