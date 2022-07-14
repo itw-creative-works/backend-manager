e1c81a2277b5fe6130bfc670ae4b751c4aaa3aa9
Ian <ian.wiedenman@gmail.com>
2022-07-14 00:32:20 -0700

    Added user:get-active-sessions
    Added user:get-active-sessions

---
+++ src/manager/functions/core/actions/api/user/get-active-sessions.js
@@ -0,0 +1,44 @@
+const _ = require('lodash')
+
+function Module() {
+
+}
+
+Module.prototype.main = function () {
+  const self = this;
+  const Manager = self.Manager;
+  const Api = self.Api;
+  const assistant = self.assistant;
+  const payload = self.payload;
+
+  return new Promise(async function(resolve, reject) {
+
+    self.Api.resolveUser({adminRequired: true})
+    .then(async (user) => {
+      const uid = _.get(user, 'auth.uid', null);
+      const id = _.get(payload.data.payload, 'id', 'sessions/app');
+
+      assistant.log(`Getting active sessions for ${uid} @ ${id}`, {environment: 'production'})
+
+      await self.libraries.admin.database().ref(id)
+      .orderByChild('uid')
+      .equalTo(uid)
+      .once('value')
+      .then(async (snap) => {
+        const data = snap.val();
+        return resolve({data: data});
+      })
+      .catch(e => {
+        return reject(assistant.errorManager(`Session query error: ${e}`, {code: 500, sentry: false, send: false, log: false}).error)
+      })
+
+    })
+    .catch(e => {
+      return reject(e);
+    })
+  });
+
+};
+
+
+module.exports = Module;
--- src/manager/functions/core/actions/api/user/sign-out-all-sessions.js
+++ src/manager/functions/core/actions/api/user/sign-out-all-sessions.js
@@ -16,24 +16,27 @@ Module.prototype.main = function () {
     self.Api.resolveUser({adminRequired: true})
     .then(async (user) => {
       const uid = _.get(user, 'auth.uid', null);
+      const id = _.get(payload.data.payload, 'id', 'sessions/app');
 
-      await self.libraries.admin.database().ref(`gatherings/online`)
+      assistant.log(`Signing out of all active sessions for ${uid} @ ${id}`, {environment: 'production'})
+
+      await self.libraries.admin.database().ref(id)
       .orderByChild('uid')
       .equalTo(uid)
       .once('value')
-      .then(async snap => {
+      .then(async (snap) => {
         const data = snap.val();
         const keys = Object.keys(data || {});
         for (var i = 0; i < keys.length; i++) {
           const key = keys[i];
           self.assistant.log(`Signing out: ${key}`, {environment: 'production'});
-          await self.libraries.admin.database().ref(`gatherings/online/${key}/command`).set('signout').catch(e => self.assistant.error(`Failed to signout ${key}`, e))
+          await self.libraries.admin.database().ref(`${id}/${key}/command`).set('signout').catch(e => self.assistant.error(`Failed to signout ${key}`, e))
           await powertools.wait(3000);
-          await self.libraries.admin.database().ref(`gatherings/online/${key}`).remove().catch(e => self.assistant.error(`Failed to delete ${key}`, e))
+          await self.libraries.admin.database().ref(`${id}/${key}`).remove().catch(e => self.assistant.error(`Failed to delete ${key}`, e))
         }
       })
       .catch(e => {
-        console.error('Gathering query error', e);
+        console.error('Session query error', e);
       })
 
       await self.libraries.admin
