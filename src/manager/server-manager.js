c1bb375a1013aea63a359ebc68326016eabd8cba
Ian <ian.wiedenman@gmail.com>
2024-07-29 19:34:34 -0700

    Update inputs

--- src/manager/server-manager.js
+++ src/manager/server-manager.js
@@ -2,25 +2,34 @@ const powertools = require('node-powertools');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
-function ServerManager() {
+function ServerManager(command, options) {
+  const self = this;
+
+  // Set the command
+  self.command = command;
+  self.options = options;
 }
 
 ServerManager.prototype.monitor = function (command, options) {
   const self = this;
 
   return new Promise(async function(resolve, reject) {
+    // Use instance properties if arguments are not provided
+    command = command || self.command;
+    options = options || self.options;
+
+    // Set the command
+    command = command || yargs(hideBin(process.argv)).argv.command || 'npm start';
+
     // Set the options
     options = options || {};
     options.log = options.log === undefined ? true : options.log;
 
-    // Set the command
-    self.command = command || yargs(hideBin(process.argv)).argv.command || 'npm start';
-
     // Log
-    console.log('Starting server...', self.command);
+    console.log('Starting server...', command);
 
     // Start the server
-    await powertools.execute(self.command, options, (serverProcess) => {
+    await powertools.execute(command, options, (serverProcess) => {
       serverProcess.on('exit', (code) => {
         if (code !== 0) {
           console.log('Server crashed. Restarting...');
