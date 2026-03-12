0ca765149dbd95759d9035ac569f717f0c9e6f59
Ian <ian.wiedenman@gmail.com>
2026-03-11 19:46:59 -0700

    No longer require remote config, remove from firebase.json

--- src/cli/commands/setup-tests/remoteconfig-template-in-json.js
+++ src/cli/commands/setup-tests/remoteconfig-template-in-json.js
@@ -2,17 +2,28 @@ const BaseTest = require('./base-test');
 const jetpack = require('fs-jetpack');
 const _ = require('lodash');
 
+const ENABLED = false;
+
 class RemoteconfigTemplateInJsonTest extends BaseTest {
   getName() {
     return 'remoteconfig template in JSON';
   }
 
   async run() {
-    return this.self.firebaseJSON?.remoteconfig?.template === 'remoteconfig.template.json';
+    if (ENABLED) {
+      return this.self.firebaseJSON?.remoteconfig?.template === 'remoteconfig.template.json';
+    }
+
+    return !this.self.firebaseJSON?.remoteconfig;
   }
 
   async fix() {
-    _.set(this.self.firebaseJSON, 'remoteconfig.template', 'remoteconfig.template.json');
+    if (ENABLED) {
+      _.set(this.self.firebaseJSON, 'remoteconfig.template', 'remoteconfig.template.json');
+    } else {
+      delete this.self.firebaseJSON.remoteconfig;
+    }
+
     jetpack.write(`${this.self.firebaseProjectPath}/firebase.json`, JSON.stringify(this.self.firebaseJSON, null, 2));
   }
 }
